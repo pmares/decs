b5d3ee0e9afbb7ec908f4a4c3b07ba6a196233c1
janmagnet <janmagnet@4df8850b-d638-0410-90ba-3157cc91a17e>
2007-09-29 18:01:16 +0000

    - Moved file and general purpose code to util.c.

---
+++ src/dlx/util.h
@@ -0,0 +1,47 @@
+#ifndef DLX_UTIL_H_
+#define DLX_UTIL_H_
+
+namespace dlx {
+
+// Forward declaration to prevent circular dependency between headers.
+class Column;
+
+#if CPU_ARCHITECTURE == BIG_ENDIAN
+
+#define SwapTwoBytes(data) ( \
+		(((data) >> 8) & 0x00FF) | \
+		(((data) << 8) & 0xFF00) )
+
+#define SwapFourBytes(data) ( \
+		(((data) >> 24) & 0x000000FF) | \
+		(((data) >> 8)  & 0x0000FF00) | \
+		(((data) << 8)  & 0x00FF0000) | \
+		(((data) << 24) & 0xFF000000) )
+
+#define SwapEightBytes(data) ( \
+		(((data) >> 56) & 0x00000000000000FF) | \
+		(((data) >> 40) & 0x000000000000FF00) | \
+		(((data) >> 24) & 0x0000000000FF0000) | \
+		(((data) >> 8)  & 0x00000000FF000000) | \
+		(((data) << 8)  & 0x000000FF00000000) | \
+		(((data) << 24) & 0x0000FF0000000000) | \
+		(((data) << 40) & 0x00FF000000000000) | \
+		(((data) << 56) & 0xFF00000000000000) )
+
+#else
+
+#define SwapTwoBytes (data) (data)
+#define SwapFourBytes (data) (data)
+#define SwapEightBytes (data) (data)
+
+#endif
+
+typedef unsigned int uint;
+
+void panic(const char* msg);
+int read_file(const char* file, Column* header);
+
+} /*namespace dlx*/
+
+
+#endif /*DLX_UTIL_H_*/
