e73d731e3bca94e707da8588c7909a4ad49f8284
janmagnet <janmagnet@4df8850b-d638-0410-90ba-3157cc91a17e>
2007-10-02 19:56:50 +0000

    - Added the nqdecs library which contains a forward transform which generates a DECS compatible matrix for the n-queens problem. - Added the degen program which is a general purpose matrix generator which links to libnqdecs.

---
+++ src/degen/degen.cpp
@@ -0,0 +1,50 @@
+/**
+ * degen - Exact cover matrix generator for the Dancing Links library.
+ * Copyright (C) 2007  Jan Magne Tjensvold
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#include <iostream>
+#include "nqdecs.h"
+
+const char* version = "1.0";
+
+using namespace std; 
+
+void print_usage() {
+	cout << "Usage: degen.exe <params> <file.decs>" << '\n';
+}
+
+int main(int argc, char* argv[]) {
+	cout << "Degen v" << version << '\n';
+	cout << "Copyright (c) 2007  Jan Magne Tjensvold" << "\n\n";
+	cout.flush();
+	
+	if (argc < 2) {
+		cerr << "No file specified on the command line" << endl;
+		print_usage();
+		return 1;
+	} else if (!strcmpi(argv[1],"-HELP")) {
+		print_usage();
+		return 0;
+	}
+	
+	cout << "Generating matrix..." << '\n';
+	setQueens(atoi(argv[1]));
+	transform(argv[2]);
+	cout << "Matrix generated and written to the file:" << '\n';
+	cout << argv[2] << endl;
+	return 0;
+}
