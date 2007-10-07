16ec19c5ec05b9562b89c80197ba75f201c4b43f
janmagnet <janmagnet@4df8850b-d638-0410-90ba-3157cc91a17e>
2007-10-07 20:30:38 +0000

    - Code beautification by adding usage_error() to handle bad command params and some whitespace niceness.

--- trunk/src/dance/dance.cpp
+++ trunk/src/dance/dance.cpp
@@ -29,27 +29,32 @@ using namespace std;
  * Prints command line options.
  */
 void print_usage() {
-	cout << "Usage: dance [OPTIONS] [FILE]\n\n";
-	cout << "Options:\n";
-	cout << "  -v                 Verbose output.\n";
-	cout << "  -q                 Quiet. Don't output to stdout.\n";
-	cout << "  --count-updates    Print the number of link updates.\n";
-	cout << "  --profile          Print the link update profile.\n";
-	cout << "  --help             Print help information and exit.\n";
-	cout << "  --version          Print program version and exit.\n\n";
-	cout << "Report bugs to <http://code.google.com/p/decs/issues/list>.";
+	cout << "Usage: dance [OPTIONS] [FILE]\n"
+			"\n"
+			"Options:\n"
+			"  -v, --verbose      Verbose output.\n"
+			"  -q, --quiet        Quiet. Don't output to stdout.\n"
+			"  --count-updates    Print the number of link updates.\n"
+			"  --profile          Print the link update profile.\n"
+			"  --help             Print help information and exit.\n"
+			"  --version          Print program version and exit.\n"
+			"\n"
+			"Report bugs to <http://code.google.com/p/decs/issues/list>.";
 }
 
 /**
  * Prints version and copyright notice.
  */
 void print_version() {
-	cout << "Dance (DECS) " << version << '\n';
-	cout << "Copyright (c) 2007 Jan Magne Tjensvold\n";
-	cout << "This is free software. You may redistribute copies of it under the terms of\n";
-	cout << "the GNU General Public License version 2 as published by the Free Software\n";
-	cout <<	"Foundation <http://www.gnu.org/licenses/gpl.html>.\n";
-	cout << "There is NO WARRANTY, to the extent permitted by law.";
+	cout << "dance (DECS toolkit) " << version << "\n"
+			"Copyright (C) 2007 Jan Magne Tjensvold\n"
+			"This is free software; See the source for copying conditions. There is NO\n"
+			"WARRANTY; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
+}
+
+void usage_error() {
+	print_usage();
+	exit(1);
 }
 
 int main(int argc, char* argv[]) {
@@ -60,48 +65,45 @@ int main(int argc, char* argv[]) {
 	
 	// Interpret command line parameters.
 	for (int i = 1; i < argc; i++) {
-		if (!strcmp(argv[i],"-q")) {
+		if (!strcmp(argv[i], "-q") || !strcmp(argv[i], "--quiet")) {
 			verbose = 0;
-		} else if (!strcmp(argv[i],"-v")) {
+		} else if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--verbose")) {
 			verbose++;
-		} else if (!strcmp(argv[i],"--help")) {
+		} else if (!strcmp(argv[i], "--help")) {
 			print_usage();
 			return 0;
-		} else if (!strcmp(argv[i],"--version")) {
+		} else if (!strcmp(argv[i], "--version")) {
 			print_version();
 			return 0;
-		} else if (!strcmp(argv[i],"--count-updates")) {
+		} else if (!strcmp(argv[i], "--count-updates")) {
 			countUpdates = true;
-		} else if (!strcmp(argv[i],"--profile")) {
+		} else if (!strcmp(argv[i], "--profile")) {
 			showProfile = true;
+		} else if (i == argc-1) {
+			file = argv[i];
 		} else {
-			if (i == argc-1) {
-				file = argv[i];
-			} else {
-				cerr << "Unknown command line parameter '" << argv[i] << "'\n";
-				print_usage();
-				return 1;
-			}
+			cerr << "Unknown command line parameter '" << argv[i] << "'\n";
+			usage_error();
 		}
 	}
 	
 	if (file == NULL) {
 		cerr << "No file specified on the command line\n";
-		print_usage();
-		return 1;
+		usage_error();
 	}
 	
-	if (verbose > 0) { 
-		cout << "Dancing to the rhythm of" << '\n';
+	if (verbose > 0) {
+		cout << "Dancing to the rhythm of\n";
 		cout << file << "\n\n";
 	}
+	
 	dlx::setVerboseLevel(verbose);
 	dlx::solve(file);
 	
 	uint updates = dlx::getUpdates();
 	
 	if (showProfile) {
-		cout << "Link update profile:\n";
+		cout << "\nLink update profile:\n";
 		uint maxLevel = dlx::getMaxLevel();
 		for (uint i = 0; i < maxLevel; i++) {
 			uint upd = dlx::getProfile(i);
@@ -109,8 +111,7 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	
-	if (countUpdates)
-		cout << "Link updates: " << updates << '\n';
+	if (countUpdates) cout << "\nTotal link updates: " << updates;
 
 	return 0;
 }
