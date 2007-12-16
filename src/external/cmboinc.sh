6571331174d5c9cb0daf14809133b37fa981eb19
janmagnet <janmagnet@4df8850b-d638-0410-90ba-3157cc91a17e>
2007-12-16 23:44:59 +0000

    - Tagging version 0.1 of the DECS GPenSIM MATLAB package.

--- src/external/cmboinc.sh
+++
@@ -1,6 +0,0 @@
-rm -f boinc/CMakeLists.txt
-rm -f boinc/api/CMakeLists.txt
-rm -f boinc/lib/CMakeLists.txt
-ln -s `pwd`/boinc_cmake/CMakeLists.txt boinc/CMakeLists.txt
-ln -s `pwd`/boinc_cmake/api/CMakeLists.txt boinc/api/CMakeLists.txt
-ln -s `pwd`/boinc_cmake/lib/CMakeLists.txt boinc/lib/CMakeLists.txt
